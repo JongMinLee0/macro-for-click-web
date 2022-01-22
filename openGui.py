db0e94f94d76bc9a279244cdb07f19c413368bce
Jongmin Lee <jongminlee0218@gmail.com>
2022-01-22 22:14:39 +0900

    feat: Develop default gui

--- openGui.py
+++ openGui.py
@@ -68,6 +68,7 @@ class ConsoleUi:
         self.scrolled_text.tag_config('WARNING', foreground='orange')
         self.scrolled_text.tag_config('ERROR', foreground='red')
         self.scrolled_text.tag_config('CRITICAL', foreground='red', underline=1)
+        
         # Create a logging handler using a queue
         self.log_queue = queue.Queue()
         self.queue_handler = QueueHandler(self.log_queue)
@@ -86,7 +87,8 @@ class ConsoleUi:
         self.scrolled_text.yview(tk.END)
 
     def poll_log_queue(self):
-        # Check every 100ms if there is a new message in the queue to display
+
+        # Check every 100ms if there is a new messaeg in the queue to display
         while True:
             try:
                 record = self.log_queue.get(block=False)
@@ -98,71 +100,75 @@ class ConsoleUi:
 
 
 class FormUi:
-
     def __init__(self, frame):
         self.frame = frame
-        # Create a combobbox to select the logging level
-        values = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
-        self.level = tk.StringVar()
-        ttk.Label(self.frame, text='Level:').grid(column=0, row=0, sticky=W)
-        self.combobox = ttk.Combobox(
-            self.frame,
-            textvariable=self.level,
-            width=25,
-            state='readonly',
-            values=values
-        )
-        self.combobox.current(0)
-        self.combobox.grid(column=1, row=0, sticky=(W, E))
-        # Create a text field to enter a message
-        self.message = tk.StringVar()
-        ttk.Label(self.frame, text='Message:').grid(column=0, row=1, sticky=W)
-        ttk.Entry(self.frame, textvariable=self.message, width=25).grid(column=1, row=1, sticky=(W, E))
+        # Create a text field 
+        self.id = tk.StringVar()
+        self.password = tk.StringVar()
+        ttk.Label(self.frame, text='아이디:').grid(column=0, row=0, sticky=W)
+        ttk.Label(self.frame, text='비밀번호:').grid(column=0, row=1, sticky=W)
+        ttk.Entry(self.frame, textvariable=self.id, width=25).grid(column=1, row=0, sticky=(W, E))
+        ttk.Entry(self.frame, textvariable=self.password, width=25).grid(column=1, row=1, sticky=(W, E))
+        
         # Add a button to log the message
-        self.button = ttk.Button(self.frame, text='Submit', command=self.submit_message)
+        self.button = ttk.Button(self.frame, text='시작', command=self.start_event)
         self.button.grid(column=1, row=2, sticky=W)
+        self.button = ttk.Button(self.frame, text='중지', command=self.stop_event)
+        self.button.grid(column=2, row=2, sticky=W)
 
-    def submit_message(self):
-        # Get the logging level numeric value
-        lvl = getattr(logging, self.level.get())
-        logger.log(lvl, self.message.get())
+    def start_event(self):
+        # Get info
+        id = self.id.get()
+        password = self.password.get()
+        # logger.log(lvl, self.message.get())
+
+    def stop_event(self):
+        #stop event
+        id = self.id.get()
 
 
 class ThirdUi:
-
     def __init__(self, frame):
         self.frame = frame
-        ttk.Label(self.frame, text='This is just an example of a third frame').grid(column=0, row=1, sticky=W)
-        ttk.Label(self.frame, text='With another line here!').grid(column=0, row=4, sticky=W)
+        ttk.Label(self.frame, text='아이디와 패스워드를 입력해주세요.').grid(column=0, row=1, sticky=W)
+        # ttk.Label(self.frame, text='With another line here!').grid(column=0, row=4, sticky=W)
+
 
 
 class App:
-
+    # GUI
     def __init__(self, root):
+        # Root Configuration
         self.root = root
-        root.title('Logging Handler')
+        root.title('Auto Click')
+        root.geometry("740x230+580+350")
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
+        root.resizable(False, False)
+
         # Create the panes and frames
         vertical_pane = ttk.PanedWindow(self.root, orient=VERTICAL)
         vertical_pane.grid(row=0, column=0, sticky="nsew")
         horizontal_pane = ttk.PanedWindow(vertical_pane, orient=HORIZONTAL)
         vertical_pane.add(horizontal_pane)
-        form_frame = ttk.Labelframe(horizontal_pane, text="MyForm")
+        form_frame = ttk.Labelframe(horizontal_pane, text="loginForm")
         form_frame.columnconfigure(1, weight=1)
         horizontal_pane.add(form_frame, weight=1)
         console_frame = ttk.Labelframe(horizontal_pane, text="Console")
         console_frame.columnconfigure(0, weight=1)
         console_frame.rowconfigure(0, weight=1)
         horizontal_pane.add(console_frame, weight=1)
-        third_frame = ttk.Labelframe(vertical_pane, text="Third Frame")
+        third_frame = ttk.Labelframe(vertical_pane, text="Info")
         vertical_pane.add(third_frame, weight=1)
+        
         # Initialize all frames
         self.form = FormUi(form_frame)
         self.console = ConsoleUi(console_frame)
         self.third = ThirdUi(third_frame)
         self.clock = Clock()
-        self.clock.start()
+        # self.clock.start()
+
+        # close event
         self.root.protocol('WM_DELETE_WINDOW', self.quit)
         self.root.bind('<Control-q>', self.quit)
         signal.signal(signal.SIGINT, self.quit)
