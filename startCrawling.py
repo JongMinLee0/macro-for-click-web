96dfc7566b414c3c6aa92b3b6ee8e8a384fb6944
Jongmin Lee <jongminlee0218@gmail.com>
2022-01-23 13:46:23 +0900

    feat: Move to login page

--- startCrawling.py
+++ startCrawling.py
@@ -19,6 +19,35 @@ ot16908
 '''
 
 
+def login(driver, id, pwd):
+    # move login page
+    login_ele = driver.find_element_by_xpath(
+        "//*[@id='header']/div[1]/div/div[1]/div[2]/ul/li[2]/a")
+    print(login_ele)
+    login_ele.click()
+
+    # input id, password
+    id_ele = driver.find_element_by_xpath(
+        "//*[@id='sub']/div/div[2]/div/div[2]/form/ul/li[1]/input")
+    id_ele.send_keys(id)
+    pwd_ele = driver.find_element_by_xpath(
+        "//*[@id='sub']/div/div[2]/div/div[2]/form/ul/li[2]/input")
+    pwd_ele.send_keys(pwd)
+
+    # click login
+    login_btn = driver.find_element_by_xpath(
+        "//*[@id='sub']/div/div[2]/div/div[2]/form/input")
+    login_btn.click()
+
+    result = driver.switch_to.alert
+    print(result.text())
+    try:
+        result = driver.switch_to.alert
+        print(result.text())
+    except:
+        print("로그인 성공")
+
+
 def startCwal(id, pwd):
     check_os = platform.system()
     # Crawling Target URL
@@ -33,7 +62,14 @@ def startCwal(id, pwd):
 
     driver = webdriver.Chrome(service=service)
     driver.get(url=URL)
+    driver.implicitly_wait(10)
+    # login
+    login(driver, id, pwd)
+
+    # block auto close for test
+    while(True):
+        pass
 
 
 if __name__ == '__main__':
-    startCwal('123', '123')
+    startCwal('ot16908', 'dlaxorbs94')
